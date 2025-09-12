9b3b630330aecaf6a1dfee1996249d89802f7d0f
HedvigS <hedvig.skirgard@gmail.com>
2025-09-12 17:24:31 +0200

    Update figureS1-datasetsize.R

--- figureS1-datasetsize.R
+++ figureS1-datasetsize.R
@@ -10,34 +10,24 @@ BTRESULTSDIR <- "../results/brms/"
 
 theme_set(theme_classic(base_size=14))
 
-df.others <- readr::read_tsv('../results/results.csv', show_col_types=FALSE)
+df.others <- readr::read_tsv('results/BT_results_summary/results.txt', show_col_types=FALSE)
 
 # cleanup
 df.others <- df.others %>%
-  select(code, Universal.shorter, `Size (review.of.original)`) %>%
-  rename(size=`Size (review.of.original)`)
+dplyr::select(Universal = code, Universal.shorter, old_lit_size = `Size..review.of.original.`) 
 
 # remove the 23 universals where we do not have recorded size
 df.others <- df.others %>% filter(!is.na(size))
 
+if(!file.exists("lgs_per_universal_counts.csv")){
+  source("count_samples_sizes.R")
+}
 
-# load from our analyses
-filelist <- list.files(BTRESULTSDIR, pattern="BT_data.txt", recursive=TRUE, full.names=TRUE)
-
-# almost as quick as `wc -l`
-df.bt <- sapply(filelist, countLines, simplify=TRUE, USE.NAMES=TRUE)
-df.bt <- data.frame(filename = names(df.bt), size = df.bt)
-df.bt$code <- basename(dirname(df.bt$filename))
-
-rownames(df.bt) <- NULL
-
-
-df.others$sample <- 'Literature'
-df.bt$sample <- 'Grambank'
+df.bt <- read_csv("lgs_per_universal_counts.csv", show_col_types = F)
 
 p <- ggplot() +
-  geom_histogram(data = df.others[c('code', 'size', 'sample')], aes(x=size, group=sample),fill = "steelblue", color = "steelblue", alpha = 0.7, bins = 30) +
-  geom_histogram(data = df.bt[c('code', 'size', 'sample')], aes(x=size, group=sample),fill = "tomato", color = "tomato", alpha = 0.7, bins = 30) +
+  geom_histogram(data = df.others[c('Universal', 'old_lit_size')], aes(x=old_lit_size),fill = "steelblue", color = "steelblue", alpha = 0.7, bins = 30) +
+  geom_histogram(data = df.bt[c('Universal', 'BT_n')], aes(x=BT_n),fill = "tomato", color = "tomato", alpha = 0.7, bins = 30) +
   xlab("Number of Languages") +
   ylab("Frequency")
 
