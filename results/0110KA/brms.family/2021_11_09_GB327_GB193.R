80a400f26291571a19a7f2676d7bdf66c8bb3eb1
SimonGreenhill <SimonGreenhill@users.noreply.github.com>
2025-09-18 21:49:38 +1200

    adding family analyses

---
+++ results/0110KA/brms.family/2021_11_09_GB327_GB193.R
@@ -0,0 +1,39 @@
+#Universal 110. There is a clear tendency for Rel N languages to be Adj N and for N Rel languages to be N Adj.
+# IF relative clauses follow their head nouns, THEN adjectives mostly follow their head nouns also.
+
+#N-Rel > N-Adj
+
+#GB327:1 > GB193:2
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+setwd("../..")
+
+GB_wide_strict <- read.csv("GB_wide_strict.tsv", sep = "\t")
+
+data_frame <- subset(x = GB_wide_strict, select = c("Language_ID", "GB193", "GB327"))
+
+data_frame_compl <- data_frame[complete.cases(data_frame),]
+
+# prepare datafile
+
+#making sure the first set of conditions is met: GB327:1
+for(i in 1:nrow(data_frame_compl)){
+  if(data_frame_compl$GB327[i] == 1)
+  {data_frame_compl$Condition_1[i] <- 1}
+  else(data_frame_compl$Condition_1[i] <- 0)
+}
+
+
+#making sure the second set of conditions is met: GB193:2
+for(i in 1:nrow(data_frame_compl)){
+  if(data_frame_compl$GB193[i] == 2)
+  {data_frame_compl$Condition_2[i] <- 1}
+  else(data_frame_compl$Condition_2[i] <- 0)
+}
+
+# write files
+data_frame_compl2 <- subset(x = data_frame_compl, select = c("Language_ID", "Condition_1", "Condition_2"))
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+write.table(data_frame_compl2, file = "BT_data.txt", sep = "\t", quote = F, row.names = F, col.names = F)
