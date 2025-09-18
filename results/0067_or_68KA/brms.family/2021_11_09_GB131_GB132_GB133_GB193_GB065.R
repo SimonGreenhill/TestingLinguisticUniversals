b6cda9e811f79bede5e1711e15dee0194179c19e
SimonGreenhill <SimonGreenhill@users.noreply.github.com>
2025-09-18 21:49:26 +1200

    adding family analyses

---
+++ results/0067_or_68KA/brms.family/2021_11_09_GB131_GB132_GB133_GB193_GB065.R
@@ -0,0 +1,37 @@
+#Universal 67. If a language has VSO word order, then if the adjective follows the noun, the genitive follows the noun.
+
+#VSO & N-Adj > N-Gen
+
+#GB131:1 & GB132:0 & GB133:0 & GB193:2 > GB065:2
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+setwd("../..")
+
+GB_wide_strict <- read.csv("GB_wide_strict.tsv", sep = "\t")
+
+data_frame <- subset(x = GB_wide_strict, select = c("Language_ID", "GB131", "GB132", "GB133", "GB193", "GB065"))
+
+data_frame_compl <- data_frame[complete.cases(data_frame),]
+
+# prepare datafile
+
+#making sure the first set of conditions is met: GB131:1 & GB132:0 & GB133:0 & GB193:2 
+for(i in 1:nrow(data_frame_compl)){
+  if(data_frame_compl$GB131[i] == 1 & data_frame_compl$GB132[i] == 0 & data_frame_compl$GB133[i] == 0 & data_frame_compl$GB193[i] == 2)
+  {data_frame_compl$Condition_1[i] <- 1}
+  else(data_frame_compl$Condition_1[i] <- 0)
+}
+
+#making sure the second set of conditions is met:  GB065:2
+for(i in 1:nrow(data_frame_compl)){
+  if(data_frame_compl$GB065[i] == 2)
+  {data_frame_compl$Condition_2[i] <- 1}
+  else(data_frame_compl$Condition_2[i] <- 0)
+}
+
+# write files
+data_frame_compl2 <- subset(x = data_frame_compl, select = c("Language_ID", "Condition_1", "Condition_2"))
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+write.table(data_frame_compl2, file = "BT_data.txt", sep = "\t", quote = F, row.names = F, col.names = F)
