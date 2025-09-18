4576668cfb3bf40ab663cfd44de714bba49155f7
SimonGreenhill <SimonGreenhill@users.noreply.github.com>
2025-09-18 21:49:40 +1200

    adding family analyses

---
+++ results/0173KA/brms.family/2021_11_09_GB074_GB025_GB065.R
@@ -0,0 +1,39 @@
+#Universal 173. If a language is prepositional, then if the demonstrative follows the noun, the genitive follows the noun.
+
+#Adp-N & N-Dem > N-Gen
+
+#GB074:1 & GB025:2 > GB065:2
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+setwd("../..")
+
+GB_wide_strict <- read.csv("GB_wide_strict.tsv", sep = "\t")
+
+data_frame <- subset(x = GB_wide_strict, select = c("Language_ID", "GB074", "GB025", "GB065"))
+
+data_frame_compl <- data_frame[complete.cases(data_frame),]
+
+# prepare datafile
+
+#making sure the first set of conditions is met: GB074:1 & GB025:2
+for(i in 1:nrow(data_frame_compl)){
+  if(data_frame_compl$GB074[i] == 1 & data_frame_compl$GB025[i] == 2)
+  {data_frame_compl$Condition_1[i] <- 1}
+  else(data_frame_compl$Condition_1[i] <- 0)
+}
+
+
+#making sure the second set of conditions is met: GB065:2
+for(i in 1:nrow(data_frame_compl)){
+  if(data_frame_compl$GB065[i] == 2)
+  {data_frame_compl$Condition_2[i] <- 1}
+  else(data_frame_compl$Condition_2[i] <- 0)
+}
+
+# write files
+data_frame_compl2 <- subset(x = data_frame_compl, select = c("Language_ID", "Condition_1", "Condition_2"))
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+write.table(data_frame_compl2, file = "BT_data.txt", sep = "\t", quote = F, row.names = F, col.names = F)
+
