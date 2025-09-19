22b4d26237480934cdf1ec6228d930dafd91314f
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2025-09-19 14:40:18 +0200

    get_glottolog_language_table rm SH.misc, switch to 4.8, output -> summary

--- get_glottolog_language_table.R
+++ get_glottolog_language_table.R
@@ -1,26 +1,19 @@
 #This script takes the values and languages tables from a cldf-release and combines then and transforms them to a wide data format from a long. It does not take into account the parameter or code tables.
 
-source("requirements.R")
+library(tidyverse)
+library(reshape2)
 
-if(!file.exists("output/processed_data/glottolog_4.3_languages.tsv")){
+if(!file.exists("summary/glottolog_4.8_languages.tsv")){
 
-  dir <- paste0("../data/glottolog")
-  if(!dir.exists(dir)){
-    dir.create(dir)}
-    
-    if(!file.exists("../data/glottolog/cldf/codes.csv")){
-      SH.misc::get_zenodo_dir(url = "https://zenodo.org/records/4061165/files/glottolog/glottolog-cldf-v4.3.zip", 
-                              exdir= "../data/glottolog/")
-  }
-  
 # fetching Glottolog v4.3 from Zenodo using rcldf (requires internet)
-glottolog_rcldf_obj <- rcldf::cldf("../data/glottolog/", load_bib = F)
+glottolog_rcldf_obj <- rcldf::cldf("https://zenodo.org/records/8131091/files/glottolog/glottolog-cldf-v4.8.zip", load_bib = F)
 
 ValueTable_wide <- glottolog_rcldf_obj$tables$ValueTable %>% 
   reshape2::dcast(Language_ID ~ Parameter_ID, value.var = "Value")
   
 glottolog_rcldf_obj$tables$LanguageTable %>% 
   dplyr::rename(Language_level_ID = Language_ID, Language_ID = ID) %>% 
-  full_join(ValueTable_wide, by = "Language_ID") %>% 
-  write_tsv("output/processed_data/glottolog_4.3_languages.tsv")
-}
+  dplyr::full_join(ValueTable_wide, by = "Language_ID") %>% 
+  readr::write_tsv("summary/glottolog_4.8_languages.tsv")
+    }
+  
