dbf0ce924c41eca25a1fe78f3dd70443bc6974e3
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2025-09-19 16:57:32 +0200

    Create 00_run_all_plots.R

---
+++ 00_run_all_plots.R
@@ -0,0 +1,13 @@
+source("requirements.R")
+source("00_brms_results_summary_tables.R")
+source("count_samples_sizes.R")
+source("figure1-barplot.R")
+source("figure2-bayesfactors.R")
+
+#supplementary figures
+source("figureS1-datasetsize.R")
+source("figureS2.R")
+source("figureS4_S6.R")
+source("figureS5_plot_ridgeplots_BRMS_effects.R")
+source("figureS7_plotting.R")
+source("figureS8_plot_bias_size_implementation.R")
